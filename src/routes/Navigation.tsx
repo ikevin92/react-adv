f182e47ed4107d7cc75b053653ef5f8c93adf09d
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-02-22 01:58:19 -0500

    update

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -29,7 +29,7 @@ export const Navigation = () => {
             <Route key={path} path={path} element={<Component />} />
           ))}
 
-          <Route path='/*' element={<Navigate to='/lazy1' replace />} />
+          <Route path='/*' element={<Navigate to={routes[0].to} replace />} />
         </Routes>
       </div>
     </BrowserRouter>
