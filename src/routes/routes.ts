08fd412d52274028ce6d674d5c95205036263d64
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-02-23 00:09:36 -0500

    lazy load basico

---
+++ src/routes/routes.ts
@@ -0,0 +1,48 @@
+import { lazy, LazyExoticComponent } from 'react';
+// import { LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+
+type JSXComponent = () => JSX.Element;
+
+interface IRoute {
+  path: string;
+  component: LazyExoticComponent<JSXComponent> | JSXComponent;
+  name: string;
+  children?: IRoute[];
+}
+
+const LazyPage1 = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "LazyPage1" */ '../01-lazyload/pages/LazyPage1'
+    ),
+);
+const LazyPage2 = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "LazyPage2" */ '../01-lazyload/pages/LazyPage2'
+    ),
+);
+const LazyPage3 = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "LazyPage3" */ '../01-lazyload/pages/LazyPage3'
+    ),
+);
+
+export const routes: IRoute[] = [
+  {
+    path: '/lazy1',
+    component: LazyPage1,
+    name: 'LazyPage-1',
+  },
+  {
+    path: '/lazy2',
+    component: LazyPage2,
+    name: 'LazyPage-2',
+  },
+  {
+    path: '/lazy3',
+    component: LazyPage3,
+    name: 'LazyPage-3',
+  },
+];
