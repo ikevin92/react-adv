a540d2d761d42839f8b7d1f758c6541e3e9c6e34
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-04-09 17:30:07 -0500

    formulario sencillo

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -2,26 +2,33 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  NavLink
+  NavLink,
 } from 'react-router-dom';
+import { RegisterPage } from '../03-forms/pages/RegisterPage';
 
 import logo from '../logo.svg';
 
 export const Navigation = () => {
   return (
     <Router>
-      <div className="main-layout">
+      <div className='main-layout'>
         <nav>
-            <img src={ logo } alt="React Logo" />
+          <img src={logo} alt='React Logo' />
           <ul>
             <li>
-              <NavLink to="/" activeClassName="nav-active" exact>Home</NavLink>
+              <NavLink to='/register' activeClassName='nav-active' exact>
+                Register Page
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
+              <NavLink to='/about' activeClassName='nav-active' exact>
+                About
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
+              <NavLink to='/users' activeClassName='nav-active' exact>
+                Users
+              </NavLink>
             </li>
           </ul>
         </nav>
@@ -29,17 +36,17 @@ export const Navigation = () => {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
-            <h1>About</h1>
+          <Route path='/register'>
+            <RegisterPage />
           </Route>
-          <Route path="/users">
+          <Route path='/users'>
             <h1>Users</h1>
           </Route>
-          <Route path="/">
+          <Route path='/'>
             <h1>Home</h1>
           </Route>
         </Switch>
       </div>
     </Router>
   );
-}
+};
